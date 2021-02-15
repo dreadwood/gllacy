854ba6bf0a493fecc70fca9cf3df79b013861e8d
srg <skabass@yandex.ru>
2021-02-16 02:05:59 +0300

    6.2 Обновляет верстку и стили для a11y

--- src/js/modules/banner.js
+++ src/js/modules/banner.js
@@ -8,6 +8,7 @@
 
   banners.forEach((banner) => {
     const container = banner.querySelector('.swiper-container');
+    const btn = banner.querySelector('.banner__btn-buy');
 
     const swiper = new Swiper(container, { // eslint-disable-line no-undef
       loop: true,
@@ -28,6 +29,7 @@
       const index = swiperObj.activeIndex;
       const slide = swiperObj.slides[index];
       document.body.style.backgroundColor = slide.dataset.bgColor;
+      btn.ariaLabel = `Купить ${slide.dataset.product}`;
     });
   });
 })();
